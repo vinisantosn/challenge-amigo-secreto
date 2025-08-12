48040f773a809c8b01abe2a0bbb2534733f439b1
Vinícius Santos <vinisantoosk@gmail.com>
2025-08-12 20:58:54 -0300

    feat(listaDeAmigos): criando função para atualizar a exibição da lista de amigos

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
-let listaDeAmigos = [];
+let amigos = [];
 
 
 function adicionarAmigo() {
     let nomeDoAmigo = document.getElementById('amigo').value
-    if(nomeDoAmigo.length != 0){
-        listaDeAmigos.push(nomeDoAmigo)
+    if (nomeDoAmigo.length != 0) {
+        amigos.push(nomeDoAmigo)
+        console.log(amigos)
         limparCampo()
-        return listaDeAmigos;
+        atualizarListaDeAmigos()
+        return amigos;
     } else {
         return alert("Por favor, insira um nome.")
     }
 }
 
-function limparCampo(){
+function atualizarListaDeAmigos() {
+    let lista = document.getElementById('listaAmigos')
+    lista.innerHTML = ""
+    if(lista.length!=0){
+        amigos.forEach(amigo => {
+            lista.innerHTML += `<li>${amigo}</li>`
+        });       
+        return lista
+    } 
+}
+
+function limparCampo() {
     let nomeDoAmigo = document.getElementById('amigo')
     return nomeDoAmigo.value = ""
+}
+function limparCampoLista() {
+    let lista = document.getElementById('listaAmigos')
+    return lista.value = ""
 }
